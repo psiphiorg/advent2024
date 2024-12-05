7a492f7251df0f117a56ba68ad529044dfce25d5
David Henderson <github@psiphi.org>
2024-12-05 01:05:51 -0700

    Add comments (and fix a typo on new line 12)
    The original regular expression included "\s*", which matches any amount of whitespace, including no whitespace.  I changed it to "\s+", which matches at least one, but possibly more, whitespace characters.
    
    With the data given, this still works because every line includes whitespace, but if there were a line that included only digits with no space, then $b would be the final digit, and $a would be all other digits.

--- advent01.pl
+++ advent01.pl
@@ -1,19 +1,25 @@
 #!perl
 
-my @a;
-my @b;
+# Part 1: Given standard in with lines containing two numbers separated by some number of spaces,
+# get the difference between the two lowest digits in each column, plus the difference between
+# the second lowest digits in each column, etc.
 
-while (<>) {
-  chomp;
-  my ($a, $b) = /^(\d*)\s*(\d*)$/;
-  push @a, $a;
-  push @b, $b;
+my @a; # First column numbers
+my @b; # Second column numbers
+
+while (<>) { # For each line from standard in
+  chomp; # Remove the newline character at the end of the line (if present)
+  my ($a, $b) = /^(\d*)\s+(\d*)$/; # Get the two collections of digits surrounding some number of spaces
+  push @a, $a; # Store the first number in the first column array
+  push @b, $b; # Store the second number in the second column array
   # print "[$_] => [$a, $b]\n";
 }
 
+# Sort both arrays so they're in numerical order
 @a = sort @a;
 @b = sort @b;
 
+# For each index in the two arrays (which have the same size), get the distance between the two digits in that index
 my $total = 0;
 foreach (0..$#a) {
   $total += abs($a[$_] - $b[$_]);
@@ -21,14 +27,18 @@ foreach (0..$#a) {
 
 print "Part 1 Total: $total\n";
 
-my %counts;
+# Part 2: For each number that appears in the first column,
+# multiply that number by the number of times it appears inithe second column.
 
+# Turn a second column of [1, 2, 2, 2, 5, 7, 7] into {1 => 1, 2 => 3, 5 => 1, 7 => 2}
+my %counts;
 foreach (@b) {
   $counts{$_}++;
 }
 
+# For each number that appears in the first column,
+# find out how many times it appears in the second column and multiply those together.
 my $total2 = 0;
-
 foreach (@a) {
   $total2 += $_ * $counts{$_};
 }
